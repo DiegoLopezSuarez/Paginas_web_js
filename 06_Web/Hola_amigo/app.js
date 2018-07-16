6f52ba07bf4a751452811e87d7565b4ae8a3a700
corchuelillo <san2nds@hotmail.com>
2018-07-16 14:01:21 +0200

    app js en web

---
+++ 06_Web/Hola_amigo/app.js
@@ -0,0 +1,28 @@
+function app () {
+
+    domNombre = document.querySelector("#nombre")
+    domBtnSaludar = document.querySelector("#btnSaludar")
+    domBtnBorrar = document.querySelector("#btnBorrar")
+
+    domNombre.addEventListener('input', leerDatos)
+    domBtnSaludar.addEventListener('click', saludar)
+    domBtnBorrar.addEventListener('click', borrar)
+
+    //  = "Pepe"
+
+    
+    function leerDatos() {
+        console.log(domNombre.value)
+    }
+
+    function saludar() {
+        console.log('Hola amigo')
+    }
+
+    function borrar () {
+        console.log('Aun no se borrar');
+    }
+
+}
+document.addEventListener('DOMContentLoaded', app)
+
