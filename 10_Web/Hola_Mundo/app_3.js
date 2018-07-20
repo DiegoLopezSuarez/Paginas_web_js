7b5dda042595f749582498617792c943cb71819a
corchuelillo <san2nds@hotmail.com>
2018-07-20 12:59:58 +0200

    uso de this

--- 10_Web/Hola_Mundo/app_3.js
+++ 10_Web/Hola_Mundo/app_3.js
@@ -22,3 +22,105 @@ document.addEventListener('DOMContentLoaded', main)
     .addEventListener('click',saludar)
 }) */
 
+// al otro dia:
+
+class Apps{
+    constructor() {
+        this.oBtnSaludar = document.querySelector('#btnSaludar')
+        this.oBtnDespedir = document.querySelector('#btnDespedir') 
+        this.asignarHandlers()
+    }
+
+    asignarHandlers() {
+        this.oBtnSaludar.addEventListener('click',saludar)
+        this.oBtnDespedir.addEventListener('click', despedir) 
+    }
+
+}
+
+
+function main() {
+   
+
+    
+
+
+    function saludar() {
+        console.log(`Hola ${amigo}`)
+    }
+  
+    function despedir() {
+        console.log(`Adios ${amigo}`)
+    }
+}
+
+document.addEventListener('DOMContentLoaded', main)
+
+// tras cambios:
+
+class Apps{
+    constructor() {
+        this.amigo = 'Pepe'
+        this.oBtnSaludar = document.querySelector('#btnSaludar')
+        this.oBtnDespedir = document.querySelector('#btnDespedir') 
+        this.asignarHandlers()
+    }
+
+    asignarHandlers() {
+        this.oBtnSaludar.addEventListener('click',saludar)
+        this.oBtnDespedir.addEventListener('click', despedir) 
+    }
+
+    saludar() {
+        console.log(`Hola ${this.amigo}`)
+    }
+  
+    despedir() {
+        console.log(`Adios ${this.amigo}`)
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', 
+        () => new Apps())
+
+    //variaciones de escribir
+
+    class Apps{
+        constructor() {
+            this.amigo = 'Pepe'
+            this.oBtnSaludar = document.querySelector('#btnSaludar')
+            this.oBtnDespedir = document.querySelector('#btnDespedir') 
+            this.asignarHandlers()
+        }
+    
+        asignarHandlers() {
+            this.oBtnSaludar.addEventListener('click',saludar)
+            this.oBtnDespedir.addEventListener('click', despedir) 
+        }
+    
+        saludar() {
+            console.log(`Hola ${this.amigo}`)
+        }
+      
+        despedir() {
+            console.log(`Adios ${this.amigo}`)
+        }
+    }
+    
+    
+    document.addEventListener('DOMContentLoaded', 
+            () => new Apps())
+    
+    // Seria lo mismo que 
+    // document.addEventListener('DOMContentLoaded', 
+    //        function () {new Apps()})
+    // en este estaria usando una funcion anonima
+    
+    
+    // Seia lo mismo que 
+    //function main() {
+    //    new Apps()
+    //}
+    // document.addEventListener('DOMContentLoaded', main)
+    
