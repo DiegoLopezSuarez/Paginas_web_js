e271bbfdfb3f009c6aca18a15dc7a6e2f199e574
corchuelillo <san2nds@hotmail.com>
2018-07-23 11:01:59 +0200

    HOLA AMIGO 2 EN WEB

---
+++ 10_Web/Hola_Mundo2/this.js
@@ -0,0 +1,30 @@
+let pepe = {
+    nombre : 'Pepe',
+    direccion: {
+        calle: 'c/ Pez',
+        poblacion: 'Madrid'
+    },
+    saludar : function () {
+        console.log(`Hola, soy ${this.nombre}`);
+    }
+}
+
+
+/* jose = pepe
+jose.saludar()
+pepe.saludar() */
+
+
+let saludarFuera = pepe.saludar
+
+// Ejemplos de EJECUCION DIRECTA de funciones
+
+pepe.saludar() // Pepe
+saludarFuera() // undefined
+saludarFuera.call(pepe) // Pepe
+
+// Ejemplos de RESPUESTA A EVENTOS
+
+setTimeout(pepe.saludar, 3000) //undefined
+/* 
+setTimeout(pepe.saludar.bind(pepe), 3000) // Pepe */
