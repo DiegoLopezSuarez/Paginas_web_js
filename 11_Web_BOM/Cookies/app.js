ff7ca0d93ac12d29654f3caddaca359dcb679250
corchuelillo <san2nds@hotmail.com>
2018-07-24 13:56:30 +0200

    cookies 2

--- 11_Web_BOM/Cookies/app.js
+++ 11_Web_BOM/Cookies/app.js
@@ -1,3 +1,22 @@
-import * as Cookie from './node_modules/js-cookie/src/js.cookie.js'
+//import * as Cookie from './node_modules/js-cookie/src/js.cookie.js'
 
-Cookie.
+import * as x from './node_modules/js-cookie/src/js.cookie.js'
+
+if (Cookies.get('NumVisitas')) {
+    // Existe la cookie NumVisitas
+} else {
+    // No existe la cookie NumVisitas
+    Cookies.set('NumVisitas', 1)
+}
+
+//Para  contabilizar visitas y que se vaya incrementando:
+
+let numVisitas =  Cookies.get('NumVisitas')
+if (numVisitas) {
+    // Existe la cookie NumVisitas
+    Cookies.set('NumVisitas', ++numVisitas)
+    console.log('Visita número', numVisitas)
+} else {
+    // No existe la cookie NumVisitas
+    Cookies.set('NumVisitas', 1)
+}
